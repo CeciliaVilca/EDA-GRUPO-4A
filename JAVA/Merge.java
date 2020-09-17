ef1318d9435ebeefc1518efd19941af0b5885d46
Bondrewd <38355408+Brian022@users.noreply.github.com>
2020-09-16 20:47:57 -0500

    Create Merge.java
    MergeSort

---
+++ JAVA/Merge.java
@@ -0,0 +1,84 @@
+package merge;
+
+public class Merge {
+
+    void merge(int arr[], int l, int m, int r) 
+    { 
+         
+        int n1 = m - l + 1; 
+        int n2 = r - m; 
+  
+        // Areglos temporales
+        int L[] = new int[n1]; 
+        int R[] = new int[n2]; 
+  
+        for (int i = 0; i < n1; ++i) 
+            L[i] = arr[l + i]; 
+        for (int j = 0; j < n2; ++j) 
+            R[j] = arr[m + 1 + j]; 
+  
+  
+        int i = 0, j = 0; 
+  
+        int k = l; 
+        while (i < n1 && j < n2) { 
+            if (L[i] <= R[j]) { 
+                arr[k] = L[i]; 
+                i++; 
+            } 
+            else { 
+                arr[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        } 
+  
+        // Copia los remanentes de L
+        while (i < n1) { 
+            arr[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+  
+        // Copia los remanentes de R
+        while (j < n2) { 
+            arr[k] = R[j]; 
+            j++; 
+            k++; 
+        } 
+    } 
+  
+    void sort(int arr[], int l, int r) 
+    { 
+        if (l < r) { 
+            int m = (l + r) / 2; 
+  
+            sort(arr, l, m); 
+            sort(arr, m + 1, r); 
+  
+            merge(arr, l, m, r); 
+        } 
+    } 
+  
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n; ++i) 
+            System.out.print(arr[i] + " "); 
+        System.out.println(); 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        int arr[] = { 12, 11, 4, 13, 5, 6, 7 }; 
+  
+        System.out.println("Array desordenado"); 
+        printArray(arr); 
+  
+        Merge ob = new Merge(); 
+        ob.sort(arr, 0, arr.length - 1); 
+  
+        System.out.println("\nArray ordenado"); 
+        printArray(arr); 
+    }
+}
