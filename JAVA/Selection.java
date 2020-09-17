a86461b39e7176bb4e287d150fef97323f4a01b9
Bondrewd <38355408+Brian022@users.noreply.github.com>
2020-09-16 21:29:25 -0500

    Add files via upload

---
+++ JAVA/Selection.java
@@ -0,0 +1,40 @@
+package selection;
+
+public class Selection {
+
+    void sort(int arr[]) 
+    { 
+        int n = arr.length; 
+   
+        for (int i = 0; i < n-1; i++) 
+        { 
+            // Encuentra el minimo 
+            int min_idx = i; 
+            for (int j = i+1; j < n; j++) 
+                if (arr[j] < arr[min_idx]) 
+                    min_idx = j; 
+  
+            // Swap primer elemento con el minimo 
+            int temp = arr[min_idx]; 
+            arr[min_idx] = arr[i]; 
+            arr[i] = temp; 
+        } 
+    } 
+    
+    void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i]+" "); 
+        System.out.println(); 
+    } 
+   
+    public static void main(String args[]) 
+    { 
+        Selection ob = new Selection(); 
+        int arr[] = {64,25,12,22,11}; 
+        ob.sort(arr); 
+        System.out.println("Sorted array"); 
+        ob.printArray(arr); 
+    } 
+}
